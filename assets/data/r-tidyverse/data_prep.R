d498b5c7cbeeffcfbca2b0248e33b7c8283914f4
Clemens Schmid <clemens@nevrome.de>
2023-07-16 15:02:01 +0200

    [r] update of data preparation and references

--- assets/data/r-tidyverse/data_prep.R
+++ assets/data/r-tidyverse/data_prep.R
@@ -1,19 +1,23 @@
 library(magrittr)
+set.seed(5678)
 
-peng <- palmerpenguins::penguins
-
-peng_prepped <- peng %>%
-  dplyr::filter(!dplyr::if_any(.fns = is.na)) %>%
+peng_prepped <- palmerpenguins::penguins %>%
+  dplyr::filter(
+    !dplyr::if_any(
+      .cols = tidyselect::everything(),
+      .fns = is.na
+    )
+  ) %>%
   tibble::add_column(., id = 1:nrow(.), .before = "species")
 
 peng_prepped %>%
   dplyr::slice_sample(n = 300) %>%
   dplyr::arrange(id) %>%
   dplyr::select(-bill_length_mm, -bill_depth_mm) %>%
-  readr::write_csv("data/penguins.csv")
+  readr::write_csv("penguins.csv")
 
 peng_prepped %>%
   dplyr::slice_sample(n = 300) %>%
   dplyr::arrange(id) %>%
   dplyr::select(id, bill_length_mm, bill_depth_mm) %>%
-  readr::write_csv("data/penguin_bills_2009.csv")
+  readr::write_csv("penguin_bills.csv")
